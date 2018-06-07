cc4594b288765db3fc935b124c4b9d45ef74cd98
Brian Anderson <ba@imperva.com>
2018-06-07 15:21:40 -0700

    updated schema files for SS to load v13 API calls

--- src/ajax/ss_schema.php
+++ src/ajax/ss_schema.php
@@ -4,9 +4,11 @@ header('Cache-Control: no-cache, must-revalidate');
 $fileType = $_GET["fileType"];
 
 if ($fileType=='WADL') {
-    print(file_get_contents('../assets/v12_application.wadl'));
+    print(file_get_contents('../assets/v13_application.wadl'));
+    //print(file_get_contents('../assets/v12_application.wadl'));
 } else if ($fileType=='XSD') {
-    print(file_get_contents('../assets/xsd0_v12_2_60.xsd'));
+    print(file_get_contents('../assets/xsd0_v13.xsd'));
+    //print(file_get_contents('../assets/xsd0_v12_2_60.xsd'));
 } else {
     print('no file found');
 }
