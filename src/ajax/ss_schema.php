21b9d0ee185a64fefed94dbb76ccaeb29506b157
Brian Anderson <brian.anderson@mbp-173944.lan>
2019-08-14 14:21:23 -0700

    add SeS v14 swagger schema to code base

--- src/ajax/ss_schema.php
+++ src/ajax/ss_schema.php
@@ -7,6 +7,8 @@ if ($version=='12') {
     print(file_get_contents('../assets/12_swagger.json'));
 } else if ($version=='13') {
     print(file_get_contents('../assets/13_swagger.json'));
+} else if ($version=='14') {
+    print(file_get_contents('../assets/14_swagger.json'));
 } else {
     print('no file found');
 }
