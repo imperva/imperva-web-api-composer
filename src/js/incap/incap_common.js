230503aff2656979e65d1df1fe80f3a6878bb44a
Brian Anderson <briananderson@Brians-Mac-mini.local>
2020-09-02 00:40:01 -0700

    fixed issue where page would not load if no incap credentials were present

--- src/js/incap/incap_common.js
+++ src/js/incap/incap_common.js
@@ -233,27 +233,29 @@ function loadCredentials(){
 /* Series of call back functions to load sub account IDs in the right location when changing API_KEYs on the UI */
 function loadSubAccounts(obj){
 	var postDataObj = getUserAuthObj($(obj).val());
-	postDataObj.page_size=100;
-	postDataObj.page_num=0;
-	if ($(obj).attr('id')=='incapAccountsList') { // API Client
-		$('#incapAccountIDList').html('<option value="'+postDataObj.account_id+'">loading...</option>');
-		//$('#get the page num id later ').val('0');
-		makeIncapCall(getSubAccountUrlByAccountId('#incapAccountsList'),'POST',loadSubAccountsResponse_APIClient,postDataObj,'set');
-	} else if ($(obj).attr('id')=='incapSitesAccountsList') { // Sites
-		$('#incapSitesAccountIDList').html('<option value="'+postDataObj.account_id+'">loading...</option>');
-		$('#incapSitesPageNum').val('0');
-		makeIncapCall(getSubAccountUrlByAccountId('#incapSitesAccountsList'),'POST',loadSubAccountsResponse_Sites,postDataObj,'set');
-	} else if ($(obj).attr('id')=='incap_migrationAction') { // Migration
-		$('#incap_migrationAction').attr('disabled','disabled');
-		$('#incap_migrationAction_accountIDList').html('<option value="'+postDataObj.account_id+'">loading...</option>').attr('disabled','disabled');
-    	$("#incap_migrationAction_sites").html('<option value="">loading...</option>').attr('disabled','disabled');
-		$('#incap_migrationAction_page_num').val('0');
-		makeIncapCall(getSubAccountUrlByAccountId('#incap_migrationAction'),'POST',loadSubAccountsResponse_Migration,postDataObj,'set');
-	} else if ($(obj).attr('id')=='incap_site_group_account_list') { // Site Group
-		$('#incap_site_group_account_ID_list').html('<option value="'+postDataObj.account_id+'">loading...</option>').attr('disabled','disabled');
-		$('#incap_site_group_page_num').val('0');
-		$("#avail_incap_group_sites").html('<option value="">loading...</option>');
-		makeIncapCall(getSubAccountUrlByAccountId('#incap_site_group_account_list'),'POST',loadSubAccountsResponse_SiteGroup,postDataObj,'set');
+	if (postDataObj!=null) {
+		postDataObj.page_size=100;
+		postDataObj.page_num=0;
+		if ($(obj).attr('id')=='incapAccountsList') { // API Client
+			$('#incapAccountIDList').html('<option value="'+postDataObj.account_id+'">loading...</option>');
+			//$('#get the page num id later ').val('0');
+			makeIncapCall(getSubAccountUrlByAccountId('#incapAccountsList'),'POST',loadSubAccountsResponse_APIClient,postDataObj,'set');
+		} else if ($(obj).attr('id')=='incapSitesAccountsList') { // Sites
+			$('#incapSitesAccountIDList').html('<option value="'+postDataObj.account_id+'">loading...</option>');
+			$('#incapSitesPageNum').val('0');
+			makeIncapCall(getSubAccountUrlByAccountId('#incapSitesAccountsList'),'POST',loadSubAccountsResponse_Sites,postDataObj,'set');
+		} else if ($(obj).attr('id')=='incap_migrationAction') { // Migration
+			$('#incap_migrationAction').attr('disabled','disabled');
+			$('#incap_migrationAction_accountIDList').html('<option value="'+postDataObj.account_id+'">loading...</option>').attr('disabled','disabled');
+			$("#incap_migrationAction_sites").html('<option value="">loading...</option>').attr('disabled','disabled');
+			$('#incap_migrationAction_page_num').val('0');
+			makeIncapCall(getSubAccountUrlByAccountId('#incap_migrationAction'),'POST',loadSubAccountsResponse_Migration,postDataObj,'set');
+		} else if ($(obj).attr('id')=='incap_site_group_account_list') { // Site Group
+			$('#incap_site_group_account_ID_list').html('<option value="'+postDataObj.account_id+'">loading...</option>').attr('disabled','disabled');
+			$('#incap_site_group_page_num').val('0');
+			$("#avail_incap_group_sites").html('<option value="">loading...</option>');
+			makeIncapCall(getSubAccountUrlByAccountId('#incap_site_group_account_list'),'POST',loadSubAccountsResponse_SiteGroup,postDataObj,'set');
+		}
 	}
 }
 
